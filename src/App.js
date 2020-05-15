660bd9e7a87af705b2d5a7c003a9ddec6e2ea1d1
Jason Baddley <jasonbaddley@gmail.com>
2020-05-15 16:49:57 -0600

    cool

--- src/App.js
+++ src/App.js
@@ -3,9 +3,34 @@ import logo from './logo.svg'
 import './App.css'
 
 function App() {
+  var options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0,
+  }
+
+  function success(pos) {
+    var crd = pos.coords
+
+    console.log('Your current position is:')
+    console.log(`Latitude : ${crd.latitude}`)
+    console.log(`Longitude: ${crd.longitude}`)
+    console.log(`More or less ${crd.accuracy} meters.`)
+    alert(`Lat: ${crd.latitude} Lng: ${crd.longitude}`)
+  }
+
+  function error(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`)
+    alert(`ERROR(${err.code}): ${err.message}`)
+  }
+  function getLocation() {
+    navigator.geolocation.getCurrentPosition(success, error, options)
+  }
   return (
     <div className="App">
-      <header className="App-header">WOW!</header>
+      <header className="App-header">
+        <button onClick={getLocation}>Send My Location</button>
+      </header>
     </div>
   )
 }
