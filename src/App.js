3fd05235a11802b6dbcf41316f443c967427711d
Jason Baddley <jasonbaddley@gmail.com>
2020-05-15 17:44:23 -0600

    sweet

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import React from 'react'
 import logo from './logo.svg'
+import Pusher from 'pusher'
 import './App.css'
+
+var pusher = new Pusher({
+  appId: '974821',
+  key: '7c9c3c1e2518fe3ab82b',
+  secret: '88a52e2c6545ff746078',
+  cluster: 'us3',
+  encrypted: true,
+})
 
 function App() {
   var options = {
@@ -16,12 +25,14 @@ function App() {
     console.log(`Latitude : ${crd.latitude}`)
     console.log(`Longitude: ${crd.longitude}`)
     console.log(`More or less ${crd.accuracy} meters.`)
-    alert(`Lat: ${crd.latitude} Lng: ${crd.longitude}`)
+
+    pusher.trigger('location', 'latlng', {
+      message: crd,
+    })
   }
 
   function error(err) {
     console.warn(`ERROR(${err.code}): ${err.message}`)
-    alert(`ERROR(${err.code}): ${err.message}`)
   }
   function getLocation() {
     navigator.geolocation.getCurrentPosition(success, error, options)
