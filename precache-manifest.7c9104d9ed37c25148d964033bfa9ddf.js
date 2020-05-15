ba1f3a73ee5aadf2bba1c6e00119ded4e5817a24
Jason Baddley <jasonbaddley@gmail.com>
2020-05-15 16:16:14 -0600

    Updates

---
+++ precache-manifest.7c9104d9ed37c25148d964033bfa9ddf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6800ada47da5482e2665ff06cd38dbb9",
+    "url": "/sendlocation/index.html"
+  },
+  {
+    "revision": "7edad30cdc528ff43f6b",
+    "url": "/sendlocation/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c283dce467b60041a241",
+    "url": "/sendlocation/static/js/2.f6260ef3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sendlocation/static/js/2.f6260ef3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7edad30cdc528ff43f6b",
+    "url": "/sendlocation/static/js/main.39009e30.chunk.js"
+  },
+  {
+    "revision": "ab6dafb3aa6725a6d441",
+    "url": "/sendlocation/static/js/runtime-main.8eee39c1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sendlocation/static/media/logo.5d5d9eef.svg"
+  }
+]);
